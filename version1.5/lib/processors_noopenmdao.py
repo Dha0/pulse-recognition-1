35f21b08856dc2d6f245cfc268114b60bbc5d2d5
fuxity <fuxity75@gmail.com>
2018-07-12 20:20:12 +0300

    clean up

--- version1.5/lib/processors_noopenmdao.py
+++ version1.5/lib/processors_noopenmdao.py
@@ -50,7 +50,7 @@ class findFaceGetPulse(object):
         self.t0 = time.time()
         self.bpms2 = []
         self.bpm2 = 0
-        self.num_of_find_face = 0
+        self.num_of_not_find_face = 0
         self.flag = 0
         dpath = resource_path("haarcascade_frontalface_alt.xml")
         if not os.path.exists(dpath):
@@ -62,11 +62,11 @@ class findFaceGetPulse(object):
         self.last_wh = np.array([0, 0])
         self.output_dim = 13
         self.trained = False
+        self.start = 0
 
         self.idx = 1
         self.idx2 = 1
         self.find_faces = True
-        self.sum1=0
         self.tracker = cv2.TrackerMIL_create()
 
     def find_faces_toggle(self):
@@ -86,9 +86,9 @@ class findFaceGetPulse(object):
         self.last_center = center
         return shift
 
-    def draw_rect(self, rect, col=(0, 255, 0)):
-        x, y, w, h = rect
-        cv2.rectangle(self.frame_out, (x, y), (x + w, y + h), col, 1)
+    # def draw_rect(self, rect, col=(0, 255, 0)):
+    #     x, y, w, h = rect
+    #     cv2.rectangle(self.frame_out, (x, y), (x + w, y + h), col, 1)
 
     def get_subface_coord(self, fh_x, fh_y, fh_w, fh_h,bbox):
         x, y, w, h = bbox
@@ -109,32 +109,6 @@ class findFaceGetPulse(object):
         self.trained = not self.trained
         return self.trained
 
-    def plot(self):
-        data = np.array(self.data_buffer).T
-        np.savetxt("data.dat", data)
-        np.savetxt("times.dat", self.times)
-        freqs = 60. * self.freqs
-        idx = np.where((freqs > 50) & (freqs < 180))
-        pylab.figure()
-        n = data.shape[0]
-        for k in range(n):
-            pylab.subplot(n, 1, k + 1)
-            pylab.plot(self.times, data[k])
-        pylab.savefig("data.png")
-        pylab.figure()
-        for k in range(self.output_dim):
-            pylab.subplot(self.output_dim, 1, k + 1)
-            pylab.plot(self.times, self.pcadata[k])
-        pylab.savefig("data_pca.png")
-
-        pylab.figure()
-        for k in range(self.output_dim):
-            pylab.subplot(self.output_dim, 1, k + 1)
-            pylab.plot(freqs[idx], self.fft[k][idx])
-        pylab.savefig("data_fft.png")
-        quit()
-
-
     def find_face(self, frame):
         self.times.append(time.time() - self.t0)
         self.frame_out = self.frame_in
@@ -146,44 +120,45 @@ class findFaceGetPulse(object):
                                                            minNeighbors=4,
                                                            minSize=(
                                                                50, 50),
-                                                            flags=cv2.CASCADE_SCALE_IMAGE))
-        print(detected)
-        if self.num_of_find_face > 5:
+                                                           flags=cv2.CASCADE_SCALE_IMAGE))
+        if self.num_of_not_find_face > 5:
             cv2.putText(self.frame_out, "Does not recognize the face well enough",
                         (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
             cv2.putText(self.frame_out, "Press 'S' to start again",
                         (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
-            self.num_of_frames = 300
+            self.num_of_frames = 400
             self.flag = 1
-        if len(detected) == 0:
-            self.num_of_find_face += 1
-        if len(detected) > 0:
-            self.num_of_find_face = 0
-            detected.sort(key=lambda a: a[-1] * a[-2])
-            if self.shift(detected[-1]) > 10:
-                self.face_rect = detected[-1]
-            x1, y1, w1, h1 = self.face_rect
-            bbox = x1, y1, w1, h1
+        if self.num_of_frames != 400:
+            if len(detected) == 0:
+                self.num_of_not_find_face += 1
+            if len(detected) > 0:
+                self.num_of_not_find_face = 0
+                detected.sort(key=lambda a: a[-1] * a[-2])
+                if self.shift(detected[-1]) > 10:
+                    self.face_rect = detected[-1]
+                if self.start == 0:
+                    x1, y1, w1, h1 = self.face_rect
+                    bbox = x1, y1, w1, h1
+                    ok = self.tracker.init(frame, bbox)
+                    self.start = 1
+                if set(self.face_rect) == set([1, 1, 2, 2]):
+                    return
+                ok, bbox = self.tracker.update(frame)
+                x, y, w, h = self.get_subface_coord(0.5, 0.16, 0.25, 0.15, bbox)
+                forehead1 = x, y, w, h
+                p1 = (int(x), int(y))
+                p2 = (int(x + w), int(y + h))
+                cv2.rectangle(frame, p1, p2, (0, 255, 0), 2, 2)
+                fobject = (frame, forehead1)
+                self.all_frames.append(fobject)
+                if len(self.all_frames) > 300:
 
-
-            ok = self.tracker.init(frame, bbox)
-            if set(self.face_rect) == set([1, 1, 2, 2]):
-                return
-            ok, bbox = self.tracker.update(frame)
-            x, y, w, h = self.get_subface_coord(0.5, 0.16, 0.25, 0.15, bbox)
-            forehead1 = x, y, w, h
-            p1 = (int(x), int(y))
-            p2 = (int(x+w), int(y+h))
-            cv2.rectangle(frame, p1, p2, (0, 255, 0), 2, 2)
-            fobject = (frame, forehead1)
-            self.all_frames.append(fobject)
-            if len(self.all_frames) > 200:
-                # self.get_selected_frames_y()
-                # self.get_selected_frames_x()
-                self.check_big_move()
-                if self.num_of_frames != 300:
-                    self.selected_frames = self.all_frames.copy()
-                    self.run()
+                    # self.get_selected_frames_x()
+                    self.check_big_move()
+                    if self.num_of_frames != 400:
+                        self.get_selected_frames_y()
+                        # self.selected_frames = self.all_frames.copy()
+                        # self.run()
 
     def check_big_move(self):
         i = 0
@@ -192,12 +167,12 @@ class findFaceGetPulse(object):
             diff = np.array(self.all_frames[i+1][1][0]) - np.array(self.all_frames[i][1][0])
             temp.append(abs(diff))
             i += 1
-        if max(temp) > 10:
+        if max(temp) > 20:
             cv2.putText(self.frame_out, "Please don't move so much ",
                         (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
             cv2.putText(self.frame_out, "Press 'S' to start again",
                         (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2, cv2.LINE_AA)
-            self.num_of_frames = 300
+            self.num_of_frames = 400
 
 
 
@@ -214,7 +189,7 @@ class findFaceGetPulse(object):
             diff = np.array(self.all_frames[0][1][0]) - np.array(self.all_frames[i][1][0])
             temp.append(abs(diff))
             i += 1
-        while j < 100:
+        while j < 200:
             min_x = temp.index(min(temp))
             self.indexs.append(min_x)
             temp[min_x] = np.math.inf
@@ -232,7 +207,7 @@ class findFaceGetPulse(object):
             diff = np.array(self.all_frames[0][1][1]) - np.array(self.all_frames[i][1][1])
             temp.append(abs(diff))
             i += 1
-        while j < 100:
+        while j < 200:
             min_y = temp.index(min(temp))
             self.indexs.append(min_y)
             temp[min_y] = np.math.inf
@@ -242,13 +217,6 @@ class findFaceGetPulse(object):
 
     def run(self):
             self.times.append(time.time() - self.t0)
-            def nextpow2(i):
-                n = 1
-                j = 0
-                while n < i:
-                    n *= 2
-                    j += 1
-                return j
             xg1 = np.zeros(len(self.selected_frames), 'double')
             xg2 = np.zeros(len(self.selected_frames), 'double')
             xg3 = np.zeros(len(self.selected_frames), 'double')
@@ -307,54 +275,9 @@ class findFaceGetPulse(object):
                 cv2.putText(self.frame_out, "pulse exist",
                             (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1,  col, 2, cv2.LINE_AA)
                 print("pulse exist")
-                self.num_of_frames = 200
+                self.num_of_frames = 300
             else:
                 cv2.putText(self.frame_out, "no pulse",
                             (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1,  col, 2, cv2.LINE_AA)
                 print("no pulse!")
-                self.num_of_frames = 200
-            # # finding the next pulse in the main roi
-            # index2 = 0
-            # pick = y1[index]
-            # f1 = 0
-            # y1[index] = 0
-            # for b in range(np.math.floor(len(f) / 15 * 0.9 + 1), np.math.floor(len(f) / 15 * 3.5)):
-            #     if f1 < y1[b]:
-            #         f1 = y1[b]
-            #         index2 = b
-            # nextPulse_1 = 60 * (index2 - 0.5) * 15 / (len(y1) - 1)
-            # next_pulse_amplitude_1 = y1[index2]
-            # print("nextPulse")
-            # print(nextPulse_1)
-
-            # this is the improvement of the algorithm that compare 3 overlapping areas,
-            # and finds peak that exists in all of them
-
-            # for a in range(np.math.floor(len(f) / 15 * 0.9 + 1), np.math.floor(len(f) / 15 * 3.5)):
-            #     if (abs(yg1[a]) > abs(yg1[a - 2]) and (yg1[a]) > abs(yg1[a + 2])) and (
-            #             abs(yg2[a]) > abs(yg2[a - 2]) and (yg2[a]) > abs(yg2[a + 2])) and (
-            #             abs(yg3[a]) > abs(yg3[a - 2]) and (yg3[a]) > abs(yg3[a + 2])):
-            #         y = max(abs(yg3[a]), abs(yg1[a]))
-            #         yg4[a] = max(y, abs(yg2[a]))
-            #     else:
-            #         x = min(abs(yg3[a]), abs(yg1[a]))
-            #         yg4[a] = min(abs(yg2[a]), x)
-            # y4 = 2 * np.abs(yg4[1:np.math.floor((myNFFT / 2) + 1)])
-            # f11 = 0
-            # for a in range(np.math.floor(len(f) / 15 * 0.9 + 1), np.math.floor(len(f) / 15 * 3.5)):
-            #     if f11 < y4[a]:
-            #         f11 = y4[a]
-            #         index1 = a
-            #
-            # f11_old = f11
-            # index1_old = index1
-            # detected_pulse_2 = 60 * (index1 - 1) * 15 / (len(y4) - 1)
-            # pulse_amplitude_2 = y4[index1]
-            # print("dete")
-            # print(detected_pulse_2)
-
-
-
-
-
-
+                self.num_of_frames = 300
